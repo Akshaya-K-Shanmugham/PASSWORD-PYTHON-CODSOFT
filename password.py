45a01cd688fbf7578bf1bbe5d13012d1ea0eca97
Akshaya-K-Shanmugham <156498605+Akshaya-K-Shanmugham@users.noreply.github.com>
2024-10-17 20:03:09 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,33 @@
+#import libraries
+import tkinter as tk
+import random
+import string
+#define function
+def generate_password():
+    password_length = int(length_entry.get())
+    
+    if password_length <= 0:
+        password_label.config(text="Please enter a valid length")
+        return
+
+    characters = string.ascii_letters + string.digits + string.punctuation
+    generated_password = ''.join(random.choice(characters) for _ in range(password_length))
+    
+    password_label.config(text="Generated Password: " + generated_password)
+    # Create a tkinter window
+window = tk.Tk()
+window.title("Password Generator")
+# Label for instructions
+instruction_label = tk.Label(window, text="Enter  length of the password:")
+instruction_label.pack()
+# Entry field for password length
+length_entry = tk.Entry(window)
+length_entry.pack()
+# Button to generate password
+generate_button = tk.Button(window, text="Generate Password", command=generate_password)
+generate_button.pack()
+# Label to display the generated password
+password_label = tk.Label(window, text="")
+password_label.pack()
+# Start the tkinter main loop
+window.mainloop()
